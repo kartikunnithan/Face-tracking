cefd39fb98e671df60be97add7a6eb7fa02b1d28
kartikunnithan <kartikunnithan738@gmail.com>
2025-05-19 18:44:55 +0400

    first commit of face tracking

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+import cv2.data
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error: Could not access the camera")
+    exit()
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Failed to capture image")
+        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (125, 100, 10), 5)
+    cv2.putText(frame, f'People Count: {len(faces)}', (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 3, cv2.LINE_AA)
+    cv2.imshow("Face detection and count", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
